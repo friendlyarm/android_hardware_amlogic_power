16d91d2bad93b91456c195e4b32d90a3eaa551c4
hmz007 <hmz007@gmail.com>
2016-12-01 13:52:58 +0800

    LOCAL_MODULE: `amlogic' ==> $(TARGET_DEVICE)
    Signed-off-by: hmz007 <hmz007@gmail.com>

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := power.amlogic
+LOCAL_MODULE := power.$(TARGET_DEVICE)
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SRC_FILES := power.c
 LOCAL_SHARED_LIBRARIES := liblog
